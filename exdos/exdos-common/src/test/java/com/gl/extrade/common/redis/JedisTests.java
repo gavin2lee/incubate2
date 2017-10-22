6978ccabd441069724f4000826c0f394fcc1cd56
gavin2lee <gavin2lee@163.com>
2017-10-22 21:45:00 +0800

    redis test

--- exdos/exdos-common/src/test/java/com/gl/extrade/common/redis/JedisTests.java
+++ exdos/exdos-common/src/test/java/com/gl/extrade/common/redis/JedisTests.java
@@ -2,11 +2,13 @@ package com.gl.extrade.common.redis;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.notNullValue;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import org.junit.After;
@@ -61,6 +63,16 @@ public class JedisTests {
 	@Test
 	public void testString() {
 		int maxSize = 10000;
+		
+		for (int i = 0; i < maxSize; i++) {
+			String key = createObjectKey((long) i, User.class);
+			jedis.del(key);
+			boolean exists = jedis.exists(key);
+
+			assertThat("exists is false", exists, is(false));
+
+		}
+		
 		for (int i = 0; i < maxSize; i++) {
 			String name = "test" + i;
 			String psword = String.valueOf(System.currentTimeMillis());
@@ -103,6 +115,16 @@ public class JedisTests {
 	@Test
 	public void testStringExpire() {
 		int maxSize = 10000;
+		
+		for (int i = 0; i < maxSize; i++) {
+			String key = createObjectKey((long) i, User.class);
+			jedis.del(key);
+			boolean exists = jedis.exists(key);
+
+			assertThat("exists is false", exists, is(false));
+
+		}
+		
 		for (int i = 0; i < maxSize; i++) {
 			String name = "test" + i;
 			String psword = String.valueOf(System.currentTimeMillis());
@@ -225,17 +247,21 @@ public class JedisTests {
 
 		do {
 			ScanResult<Tuple> result = jedis.zscan(key, cursor);
-			
+
 			cursor = result.getStringCursor();
 
 			System.out.println("size:" + result.getResult().size());
-			System.out.println("cursor:"+cursor);
+			System.out.println("cursor:" + cursor);
 
 			for (Tuple t : result.getResult()) {
 				jedis.zrem(key, t.getElement());
 			}
-			
-			if(cursor.equals("0")){
+
+			if (cursor.equals("0")) {
+				ScanResult<Tuple> result0 = jedis.zscan(key, cursor);
+				for (Tuple t : result0.getResult()) {
+					jedis.zrem(key, t.getElement());
+				}
 				break;
 			}
 		} while (true);
@@ -265,16 +291,20 @@ public class JedisTests {
 		cursor = "0";
 		do {
 			ScanResult<Tuple> result = jedis.zscan(key, cursor);
-			
+
 			cursor = result.getStringCursor();
-			System.out.println("cursor:"+cursor);
+			System.out.println("cursor:" + cursor);
 			System.out.println("size:" + result.getResult().size());
 
 			for (Tuple t : result.getResult()) {
 				jedis.zrem(key, t.getElement());
 			}
-			
-			if(cursor.equals("0")){
+
+			if (cursor.equals("0")) {
+				ScanResult<Tuple> result0 = jedis.zscan(key, cursor);
+				for (Tuple t : result0.getResult()) {
+					jedis.zrem(key, t.getElement());
+				}
 				break;
 			}
 		} while (true);
@@ -282,6 +312,43 @@ public class JedisTests {
 		count = jedis.zcard(key);
 
 		assertThat("the count should be zero", count, equalTo(0L));
+	}
+
+	@Test
+	public void testHash() {
+		long maxSize = 10000;
+		for (long i = 0; i < maxSize; i++) {
+			User u = new User(i,"hash"+i, String.valueOf(System.currentTimeMillis()));
+			
+			String key = createHashKey(u);
+					
+			jedis.hmset(key, convertObjToStringMap(u));
+		}
+		
+		for(long i = 0; i < maxSize; i++){
+			String key = createHashKey(i, User.class);
+			long ret = jedis.del(key);
+			
+			assertThat("ret equals to 1", ret, equalTo(1L));
+		}
+	}
+	
+	@Test
+	public void testHashAndStringWhenKeyConfliction(){
+		long oid = 100;
+		User u = new User(oid,"user"+oid, String.valueOf(System.currentTimeMillis()));
+		String uJson = JsonUtils.toJsonString(u);
+		
+		Map<String,String> hashVal = convertObjToStringMap(u);
+		
+		String strKey = createObjectKey(u);
+		String hashKey = createHashKey(u);
+		
+		assertThat("strKey equals to hashKey", strKey, not(equalTo(hashKey)));
+		
+		jedis.set(strKey, uJson);
+		jedis.hmset(hashKey, hashVal);
+		
 	}
 
 	@Test
@@ -334,4 +401,21 @@ public class JedisTests {
 		return "sortedSet:" + clz.getSimpleName();
 	}
 
+	private String createHashKey(User user) {
+		return "hash:"+User.class.getSimpleName() + ":" + user.getOid();
+	}
+	
+	
+	private String createHashKey(long  oid,Class<?> clz){
+		return "hash:"+clz.getSimpleName()+":"+oid;
+	}
+	
+	private Map<String,String> convertObjToStringMap(User u){
+		Map<String,String> objMap = new HashMap<String,String>();
+		objMap.put("oid", String.valueOf(u.getOid()));
+		objMap.put("uname", u.getUname());
+		objMap.put("psword", u.getPsword());
+		
+		return objMap;
+	}
 }
