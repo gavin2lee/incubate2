0a434eafdd2129af24c0c3e65c44d4f8cc9d0dcb
gavin2lee <gavin2lee@163.com>
2017-11-13 22:02:46 +0800

    bin

---
+++ exdos/exdos-common/etc/xbin/mongo-startup.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+MONGODB_PATH=/opt/mongo/mongo-3.4.10/mongodb/bin
+MONGOD_EXEC=$MONGODB_PATH/mongod
+
+MONGODB_CONFIG=./mongodb.conf
+
+
+
+if [ -z $1 ];then
+	echo "to start mongodb via shell script"
+	$MONGOD_EXEC --config $MONGODB_CONFIG
+fi
+
+for args in $@
+do
+        echo $args
+done
+
+if [ "stop" = "$1" ];then
+	echo "to shutdown mongodb via shell script"
+	sudo $MONGOD_EXEC --config $MONGODB_CONFIG --shutdown
+fi
